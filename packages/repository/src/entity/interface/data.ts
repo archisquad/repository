208d8ca5b082d47bc86b7c161a66938667c2a2c3
Mateusz Gostański <mateusz.gostanski@gmail.com>
2023-10-24 19:35:02 +0200

    :art: feat: simplify EntitySchema

--- packages/repository/src/entity/interface/data.ts
+++ packages/repository/src/entity/interface/data.ts
@@ -1,9 +1,9 @@
+import { Simplify } from "type-fest"
+
 export type UserDefinedSchema = Record<string, any>
 
 export type EntitySchema<TInput extends UserDefinedSchema = UserDefinedSchema> =
-  TInput["id"] extends unknown
-    ? Omit<TInput, "id"> & { id: string }
-    : TInput & { id: string }
+  Simplify<Omit<TInput, "id"> & { id: string }>
 
 export type AllowedEntityInput<TSchema = EntitySchema> = Partial<
   Omit<TSchema, "id">
