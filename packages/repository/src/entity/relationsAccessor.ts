80a555217856b646f0b964d99e0c349dc8148091
Mateusz Gostański <mateusz.gostanski@gmail.com>
2023-11-27 10:54:48 +0100

    :recycle: refactor: relationsAccessor

--- packages/repository/src/entity/relationsAccessor.ts
+++ packages/repository/src/entity/relationsAccessor.ts
@@ -1,20 +1,21 @@
-/* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
-import { Relations, Relationship } from "./interface"
+import {
+  EntitySchema,
+  RelationshipsDefinitions,
+  ResolvedRelations,
+} from "./interface"
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export function relationAccessorFactory<const TDefinitionsUnion>(
-  definitions: readonly TDefinitionsUnion[]
-) {
-  if (definitions.length === 0) {
+export function relationAccessorFactory<
+  TSchema extends EntitySchema,
+  const TDefinitions extends RelationshipsDefinitions<TSchema>,
+>(definitions: TDefinitions) {
+  if (Object.keys(definitions).length === 0) {
     throw new Error("Relation definitions array cannot be empty")
   }
 
-  type Definitions = (typeof definitions)[number]
-
-  return definitions.reduce(
-    (accessor, definition: TDefinitionsUnion) => {
-      isDefinition(definition)
-      accessor[definition.id] = () => {
+  return Object.entries(definitions).reduce(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    (accessor, [name, definition]) => {
+      accessor[name] = () => {
         // TODO: Add implementation for reaching out to RepositoryManager
 
         return {}
@@ -22,11 +23,5 @@ export function relationAccessorFactory<const TDefinitionsUnion>(
       return accessor
     },
     {} as Record<string, any>
-  ) as Relations<Definitions>
-}
-
-function isDefinition(
-  x: unknown
-): asserts x is Relationship<Record<string, any>> {
-  return
+  ) as ResolvedRelations<TSchema, TDefinitions>
 }
