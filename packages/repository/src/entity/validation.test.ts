f67f2f086f771d03ba46d979aa07a1e0506b9188
Mateusz Gostański <mateusz.gostanski@gmail.com>
2023-12-01 20:11:16 +0100

    :sparkles: feat: add checking identifier during validation

--- packages/repository/src/entity/validation.test.ts
+++ packages/repository/src/entity/validation.test.ts
@@ -1,98 +1,73 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { validateInput } from "./validation"
 import { describe, expect, it } from "vitest"
 
+function makeTestObj(
+  {
+    schema,
+    methods,
+    relations,
+    identifier,
+  }: Partial<{
+    schema: Record<string, any>
+    methods: Record<string, any>
+    relations: Record<string, any>
+    identifier: string | ((data: any) => any)
+    // eslint-disable-next-line unicorn/no-object-as-default-parameter
+  }> = { schema: {}, methods: {}, relations: {}, identifier: "id" }
+) {
+  return {
+    schema: {
+      id: "string",
+      name: "string",
+      ...schema,
+    },
+    inferSchema: (data: any) => data,
+    methods,
+    relations,
+    identifier,
+  }
+}
+
 describe("Entity model valitation", () => {
   it.each([
+    [makeTestObj(), true],
+    [makeTestObj({ methods: { methodA: () => {} } }), true],
+    [makeTestObj({ methods: { name: () => "string" } }), false],
     [
-      {
-        schema: {
-          id: "string",
-          name: "string",
-        },
-        methods: {},
-        relations: {},
-      },
+      makeTestObj({
+        methods: { methodB: () => "string" },
+        relations: { relationA: {} },
+      }),
       true,
     ],
     [
-      {
-        schema: {
-          id: "string",
-          name: "string",
-        },
-        methods: {
-          methodA: () => {},
-        },
-        relations: {},
-      },
-      true,
-    ],
-    [
-      {
-        schema: {
-          id: "string",
-          name: "string",
-        },
-        methods: {
-          name: () => "string",
-        },
-        relations: {},
-      },
+      makeTestObj({
+        methods: { methodB: () => "string" },
+        relations: { name: {} },
+      }),
       false,
     ],
     [
-      {
-        schema: {
-          id: "string",
-          name: "string",
-        },
-        methods: {
-          methodB: () => "string",
-        },
-        relations: {
-          relationA: {},
-        },
-      },
-      true,
-    ],
-    [
-      {
-        schema: {
-          id: "string",
-          name: "string",
-        },
-        methods: {
-          methodB: () => "string",
-        },
-        relations: {
-          name: {},
-        },
-      },
+      makeTestObj({
+        methods: { relationC: () => "string" },
+        relations: { relationC: {} },
+      }),
       false,
     ],
-    [
-      {
-        schema: {
-          id: "string",
-          name: "string",
-        },
-        methods: {
-          relationC: () => "string",
-        },
-        relations: {
-          relationC: {},
-        },
-      },
-      false,
-    ],
-  ])("should validate input", ({ schema, methods, relations }, successful) => {
+    [makeTestObj({ identifier: "foo" }), false],
+    [makeTestObj({ identifier: (data: any) => data.id }), true],
+  ])("should validate input", (config, successful) => {
     if (successful) {
-      expect(() =>
-        validateInput(schema, methods, relations as any)
-      ).not.toThrow()
+      expect(() => validateInput(config)).not.toThrow()
     } else {
-      expect(() => validateInput(schema, methods, relations as any)).toThrow()
+      expect(() => validateInput(config)).toThrow()
     }
   })
 })
