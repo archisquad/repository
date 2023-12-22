86ec0c7d39a9f4c951dee2af2fd664a3e1b37bb3
Mateusz Gostański <mateusz.gostanski@gmail.com>
2023-12-22 17:17:44 +0100

    :recycle: refactor: move sut instance creation in createSut()

--- packages/repository/src/entity/proto.test.ts
+++ packages/repository/src/entity/proto.test.ts
@@ -9,7 +9,8 @@ import {
   vi,
 } from "vitest"
 import { internalEntityFactory } from "./proto"
-import { DeepReadonly } from "./types"
+import type { DeepReadonly } from "./types"
+import type { PartialDeep } from "type-fest"
 
 describe("proto", () => {
   beforeEach(() => {
@@ -19,38 +20,42 @@ describe("proto", () => {
   function createSut(
     context: TestContext,
     overrides?: {
+      inputData?: PartialDeep<TestEntityData>
       validatorFn?: (...args: any[]) => any
       identifierFn?: (...args: any[]) => any
     }
   ) {
     const { passThroughValidator, syncKeys, fakeData } = context
-    const { validatorFn = passThroughValidator, identifierFn = () => "1" } =
-      overrides ?? {}
+    const {
+      validatorFn = passThroughValidator,
+      identifierFn = () => "1",
+      inputData = {},
+    } = overrides ?? {}
 
-    const sut = internalEntityFactory<TestEntityData, "id">(
+    const sutFactory = internalEntityFactory<TestEntityData, "id">(
       syncKeys,
       validatorFn,
       identifierFn
     )
 
+    const sut = new sutFactory({ ...fakeData, ...inputData } as TestEntityData)
+
     return {
       sut,
-      inputData: fakeData,
       syncKeys,
     }
   }
 
   it("Update method returns new instance with extended data", (context) => {
-    const { sut, inputData } = createSut(context)
-    const entity = new sut({ ...inputData, id: "1" })
+    const { sut } = createSut(context)
 
-    const result = entity.update({
+    const result = sut.update({
       foo: "foo2",
       bar: 2,
       some: true,
     })
 
-    expect(result).not.toBe(entity)
+    expect(result).not.toBe(sut)
     expect(result.toObject()).toEqual(
       expect.objectContaining({
         id: "1",
@@ -73,31 +78,27 @@ describe("proto", () => {
   })
 
   it("isSynced method return status for given SyncKey", (context) => {
-    const { sut, inputData, syncKeys } = createSut(context)
+    const { sut, syncKeys } = createSut(context)
 
-    const entity = new sut({ ...inputData, id: "2" })
-
-    expect(entity.isSynced(syncKeys[0])).toBe(false)
-    expect(entity.isSynced(syncKeys[1])).toBe(false)
+    expect(sut.isSynced(syncKeys[0])).toBe(false)
+    expect(sut.isSynced(syncKeys[1])).toBe(false)
   })
 
   it("setSynced method set status for given SyncKey", async (context) => {
-    const { sut, inputData, syncKeys } = createSut(context)
-    const entity = new sut({ ...inputData, id: "1" })
+    const { sut, syncKeys } = createSut(context)
     const promise = Promise.resolve()
 
-    entity.setSynced(syncKeys[0], promise)
+    sut.setSynced(syncKeys[0], promise)
     await promise
 
-    expect(entity.isSynced(syncKeys[0])).toBe(true)
-    expect(entity.isSynced(syncKeys[1])).toBe(false)
+    expect(sut.isSynced(syncKeys[0])).toBe(true)
+    expect(sut.isSynced(syncKeys[1])).toBe(false)
   })
 
   it("getIdentifier method returns identifier value", (context) => {
-    const { sut, inputData } = createSut(context)
-    const entity = new sut({ ...inputData, id: "1" })
+    const { sut } = createSut(context)
 
-    expect(entity.getIdentifier()).toBe("1")
+    expect(sut.getIdentifier()).toBe("1")
   })
 
   it("Validator function is called on entity creation", (context) => {
@@ -105,12 +106,10 @@ describe("proto", () => {
       ...context.passThroughValidator(input),
       bar: "bar",
     }))
-    const { sut, inputData } = createSut(context, { validatorFn })
-
-    const result = new sut({ ...inputData, id: "1" })
+    const { sut } = createSut(context, { validatorFn })
 
     expect(validatorFn).toBeCalledTimes(1)
-    expect(result.data.bar).toBe("bar")
+    expect(sut.data.bar).toBe("bar")
   })
 
   it("Validator function is called on entity update", (context) => {
@@ -118,22 +117,20 @@ describe("proto", () => {
       ...context.passThroughValidator(input),
       foo: "updated",
     }))
-    const { sut, inputData } = createSut(context, { validatorFn })
-    const entity = new sut({ ...inputData, id: "1" })
+    const { sut } = createSut(context, { validatorFn })
 
-    entity.update({ foo: "foo2", bar: 2, some: true })
+    sut.update({ foo: "foo2", bar: 2, some: true })
 
     expect(validatorFn).toBeCalledTimes(2)
-    expect(entity.data.foo).toBe("updated")
+    expect(sut.data.foo).toBe("updated")
   })
 
   it("toObject should return a copy of data", (context) => {
-    const { sut, inputData } = createSut(context)
-    const entity = new sut({ ...inputData, id: "1" })
+    const { sut } = createSut(context)
 
-    const result = entity.toObject()
+    const result = sut.toObject()
 
-    expect(result).not.toBe(entity.data)
-    expect(result).toEqual(entity.data)
+    expect(result).not.toBe(sut.data)
+    expect(result).toEqual(sut.data)
   })
 })
