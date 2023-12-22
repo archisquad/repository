bff8ad17a06ced5a2e9fca42747707a0e81d25e5
Mateusz Gostański <mateusz.gostanski@gmail.com>
2023-12-22 14:38:37 +0100

    :recycle: refactor: extract condition to fn

--- packages/repository/src/entity/deepReadonly.ts
+++ packages/repository/src/entity/deepReadonly.ts
@@ -1,25 +1,40 @@
 import { DeepReadonly } from "./types"
 
-function validateCustomTypes<TValue>(value: TValue): boolean {
-  const excludedTypes = [Date, RegExp, Map, Set]
-  return !excludedTypes.some((type) => value instanceof type)
-}
-
 export function deepReadonly<TWriteableObject extends Record<string, any>>(
   obj: TWriteableObject
 ): DeepReadonly<TWriteableObject> {
   Reflect.ownKeys(obj).forEach((key) => {
     const value = obj[key as keyof TWriteableObject]
-    if (
-      value &&
-      typeof value === "object" &&
-      validateCustomTypes(value) &&
-      Object.isFrozen(value) === false
-    ) {
+    if (checkIsObject(value)) {
       // @ts-expect-error - we know that value is object
       obj[key as keyof TWriteableObject] = deepReadonly(value)
     }
   })
 
   return Object.freeze(obj) as DeepReadonly<TWriteableObject>
+}
+
+function checkIsObject(value: unknown): value is object {
+  if (!value) {
+    return false
+  }
+
+  if (typeof value !== "object") {
+    return false
+  }
+
+  if (!validateCustomTypes(value)) {
+    return false
+  }
+
+  if (Object.isFrozen(value)) {
+    return false
+  }
+
+  return true
+}
+
+function validateCustomTypes<TValue>(value: TValue): boolean {
+  const excludedTypes = [Date, RegExp, Map, Set]
+  return !excludedTypes.some((type) => value instanceof type)
 }
