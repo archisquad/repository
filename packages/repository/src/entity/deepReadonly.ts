d97f3d6981160b6c0cbe768a2c7698fd2cd45cdb
Mateusz Skrobiś <m.skrobis@evionica.com>
2024-02-09 19:23:11 +0100

    :recycle: refactor: add fn's exclude in DeepReadonly type

--- packages/repository/src/entity/deepReadonly.ts
+++ packages/repository/src/entity/deepReadonly.ts
@@ -1,9 +1,11 @@
 export type DeepReadonly<TData> = {
-  readonly [Key in keyof TData]: TData[Key] extends Record<string, any>
-    ? TData[Key] extends Set<any> | Map<any, any> | Date | RegExp
-      ? TData[Key]
-      : DeepReadonly<TData[Key]>
-    : TData[Key]
+  readonly [Key in keyof TData]: TData[Key] extends (...args: any[]) => any
+    ? TData[Key]
+    : TData[Key] extends Record<string, any>
+      ? TData[Key] extends Set<any> | Map<any, any> | Date | RegExp
+        ? TData[Key]
+        : DeepReadonly<TData[Key]>
+      : TData[Key]
 }
 
 export function deepReadonly<TWriteableObject extends Record<string, any>>(
